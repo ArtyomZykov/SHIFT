73f31e729cfe2442e93105eefc96a10b60f61b17
Зыков Артём Алексеевич <zykov.artiom15@mail.ru>
2021-10-20 13:55:42 +0700

    added fragments

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fragments.ui.fragments.FirstFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, FirstFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
