414e02b58d4e206b95bb7a15aefeffee5934178f
Зыков Артём Алексеевич <zykov.artiom15@mail.ru>
2021-10-21 18:56:06 +0700

    Сервис считывающий в бекграунд потоке данные и выдающий результат в активность

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -1,22 +1,42 @@
 package com.example.fragments
 
+
+import android.app.ActivityManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
-import com.example.fragments.ui.fragments.FirstFragment
+import android.content.Intent
+import com.example.fragments.service.SortService
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.DelicateCoroutinesApi
+
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
+    @DelicateCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                replace<FirstFragment>(R.id.container)
-                setReorderingAllowed(true)
-                addToBackStack("FirstFragment")
-            }
-        }
+        APP_ACTIVITY = this
+        startButton.setOnClickListener { startSortService() }
+        stopButton.setOnClickListener { stopSortService() }
     }
+
+
+    @DelicateCoroutinesApi
+    private fun stopSortService() {
+        stopService(Intent(this, SortService::class.java))
+        resultTextView.text = "SortService was be stopped"
+    }
+
+    @DelicateCoroutinesApi
+    fun startSortService() {
+        startService(Intent(this, SortService::class.java))
+        resultTextView.text = "processing"
+    }
+
+}
+
+fun printResultArray(array: Array<Int>) {
+    val oneToThree = array[0].toString() + "\n" + array[0].toString() + "\n" + array[0].toString()
+    APP_ACTIVITY.resultTextView.text = oneToThree
 }
---
+++ app/src/main/java/com/example/fragments/constants.kt
@@ -0,0 +1,3 @@
+package com.example.fragments
+
+lateinit var APP_ACTIVITY: MainActivity
