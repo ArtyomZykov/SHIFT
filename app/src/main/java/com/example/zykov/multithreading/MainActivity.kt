dbb237a4a97568d949bdd858bade4f9c26ba7de2
Зыков Артём Алексеевич <zykov.artiom15@mail.ru>
2021-11-18 15:07:41 +0700

    t

--- app/src/main/java/com/example/zykov/multithreading/MainActivity.kt
+++ app/src/main/java/com/example/zykov/multithreading/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.zykov.multithreading
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val timeThread = Thread {
+        for (time in 0..1000000) {
+            runOnUiThread {
+                result_text.text = time.toString()
+            }
+            Thread.sleep(1000)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        timeThread.start()
     }
+
 }
