c7056ebe063d0d1b44e4805a678dbe4b095b253f
Зыков Артём Алексеевич <zykov.artiom15@mail.ru>
2021-11-24 18:04:55 +0700

    fix

--- app/src/main/java/com/example/zykov/networkapp/MainActivityViewModel.kt
+++ app/src/main/java/com/example/zykov/networkapp/MainActivityViewModel.kt
@@ -1,9 +1,7 @@
 package com.example.zykov.networkapp
 
-import android.app.Application
 import android.os.Environment
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.zykov.networkapp.retrofit.RetrofitInstance.api
--- app/src/main/java/com/example/zykov/networkapp/retrofit/RetrofitAPI.kt
+++ app/src/main/java/com/example/zykov/networkapp/retrofit/RetrofitAPI.kt
@@ -1,8 +1,6 @@
 package com.example.zykov.networkapp.retrofit
 
 import okhttp3.MultipartBody
-import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
