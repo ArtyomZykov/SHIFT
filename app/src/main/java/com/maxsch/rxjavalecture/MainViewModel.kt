f30675b424008faf0a134d453607cc52953ed6eb
Зыков Артём Алексеевич <zykov.artiom15@mail.ru>
2021-12-03 11:27:53 +0700

    clear

--- app/src/main/java/com/maxsch/rxjavalecture/MainViewModel.kt
+++ app/src/main/java/com/maxsch/rxjavalecture/MainViewModel.kt
@@ -21,58 +21,65 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.android.awaitFrame
 
 class MainViewModel(
-	private val catsApi: CatsApi = CatsApiImpl(),
-	private val dogsApi: DogsApi = DogsApiImpl(),
-	private val ratsApi: RatsApi = RatsApiImpl(),
-	private val priceApi: PriceApi = PriceApiImpl(),
+    private val catsApi: CatsApi = CatsApiImpl(),
+    private val dogsApi: DogsApi = DogsApiImpl(),
+    private val ratsApi: RatsApi = RatsApiImpl(),
+    private val priceApi: PriceApi = PriceApiImpl(),
 ) : ViewModel() {
 
-	private val _result = MutableLiveData<Map<Animal, Int>>()
-	val result: LiveData<Map<Animal, Int>> = _result
+    private val _result = MutableLiveData<Map<Animal, Int>>()
+    val result: LiveData<Map<Animal, Int>> = _result
 
+    fun getData() {
 
-	fun getData() {
-		try {
-			viewModelScope.launch(Dispatchers.IO) {
-				var dogsList: List<Dog> = emptyList()
-				var catsList: List<Cat> = emptyList()
-				var ratsList: List<Rat> = emptyList()
+        try {
 
-				val dogsJob = async {
-					dogsList = dogsApi.getDogs()
-					Log.i("Test", "getDogs")
-				}
-				val catsJob = async {
-					catsList = catsApi.getCats()
-					Log.i("Test", "getCats")
-				}
-				val ratsJob = async {
-					ratsList = ratsApi.getRats()
-					Log.i("Test", "getRats")
-				}
+            viewModelScope.launch(Dispatchers.IO) {
 
-				awaitFrame()
-				val animalsList: List<Animal> = dogsList + catsList + ratsList
-				var animalsPricesMap: Map<Animal, Int> = emptyMap()
-				val priceJob = async {
-					for (item in animalsList) {
-						Log.i("Test2", item.toString())
-						animalsPricesMap += mapOf(item to priceApi.getPrice(item))
-					}
-				}
-				awaitFrame()
-				handleAnimalPrices(animalsPricesMap)
-			}
-		} catch (error: IllegalArgumentException) {
-			handleLoadingError(error)
-		}
-	}
+                var dogsList: List<Dog> = emptyList()
+                var catsList: List<Cat> = emptyList()
+                var ratsList: List<Rat> = emptyList()
 
-	private fun handleAnimalPrices(animalToPriceMap: Map<Animal, Int>) {
-		_result.postValue(animalToPriceMap)
-	}
+                val dogsJob = async {
+                    dogsList = dogsApi.getDogs()
+                    Log.i("Test", "getDogs")
+                }
+                val catsJob = async {
+                    catsList = catsApi.getCats()
+                    Log.i("Test", "getCats")
+                }
+                val ratsJob = async {
+                    ratsList = ratsApi.getRats()
+                    Log.i("Test", "getRats")
+                }
 
-	private fun handleLoadingError(error: IllegalArgumentException) {
-		Log.e("MainViewModel", "Failed to load animals and prices", error)
-	}
+                awaitFrame()
+
+                val animalsList: List<Animal> = dogsList + catsList + ratsList
+                var animalsPricesMap: Map<Animal, Int> = emptyMap()
+
+                val priceJob = async {
+                    for (item in animalsList) {
+                        Log.i("Test2", item.toString())
+                        animalsPricesMap += mapOf(item to priceApi.getPrice(item))
+                    }
+                }
+
+                awaitFrame()
+
+                handleAnimalPrices(animalsPricesMap)
+
+            }
+        } catch (error: IllegalArgumentException) {
+            handleLoadingError(error)
+        }
+    }
+
+    private fun handleAnimalPrices(animalToPriceMap: Map<Animal, Int>) {
+        _result.postValue(animalToPriceMap)
+    }
+
+    private fun handleLoadingError(error: IllegalArgumentException) {
+        Log.e("MainViewModel", "Failed to load animals and prices", error)
+    }
 }
